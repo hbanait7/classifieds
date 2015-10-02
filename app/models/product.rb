98bc9de44f78056316d172ff1df5b96e6a99c2c2
hbanait <hbanait7@gmail.com>
2015-10-02 13:43:28 -0400

    Add validations to product and user models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
 
+  validates :name, :price, :category, :description, presence: true
+
   belongs_to :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
 
+  before_save { self.email = email.downcase }
+
   has_secure_password
+
+  validates :name, :email, :password, :password_confirmation, presence: true
+
+  validates :password, length: { minimum: 6 }
 
   has_many :products
 
