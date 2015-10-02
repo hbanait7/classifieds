9f95049f9e32fded78b09c9b489d2506b3a4222a
hbanait <hbanait7@gmail.com>
2015-10-02 14:30:29 -0400

    Allow user to login and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ActiveRecord::Base
 
   has_many :products
 
+  # https://www.railstutorial.org/book/log_in_log_out#sec-testing_layout_changes
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+
 end
