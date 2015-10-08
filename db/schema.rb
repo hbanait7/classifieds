4d8bc6a8f5ba4bd6d238a2a7c1f7fd9a705d380a
hbanait <hbanait7@gmail.com>
2015-10-08 17:45:49 -0400

    Remove category field from product table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008155933) do
+ActiveRecord::Schema.define(version: 20151008214513) do
 
   create_table "books", force: :cascade do |t|
     t.string   "author"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20151008155933) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "price"
-    t.string   "category"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
